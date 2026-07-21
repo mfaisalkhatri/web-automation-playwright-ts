7893585b36ab9fc4776b6cfe446b88c7c056aa4e
mfaisalkhatri <mohammadfaisalkhatri@gmail.com>
2026-07-21 17:08:02 +0530

    added examples for browser, browsercontext and page

--- tests/browser-example.spec.ts
+++ tests/browser-example.spec.ts
@@ -14,3 +14,31 @@ test("browser, browsercontext and page example", async ({}) => {
   await context.close();
   await browser.close();
 });
+
+test("browser example", async ({}) => {
+  const browser = await chromium.launch({ headless: false });
+
+  await browser.close();
+});
+
+test("browser context example", async ({}) => {
+  const browser = await chromium.launch({ headless: false });
+
+  const customerContext = await browser.newContext();
+  const adminContext = await browser.newContext();
+
+  await customerContext.close();
+  await adminContext.close();
+  await browser.close();
+});
+
+test('Multiple page in the same context', async({}) => {
+const browser = await chromium.launch({headless: false});
+const context = await browser.newContext();
+
+const homePage = await context.newPage();
+const docsPage = await context.newPage();
+
+await homePage.goto("https://playwright.dev");
+await docsPage.goto("https://playwright.dev/docs");
+});
