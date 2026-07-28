c1f83014b1a988e477facbd86d2f6a402d5173ed
mfaisalkhatri <mohammadfaisalkhatri@gmail.com>
2026-07-28 08:02:26 +0530

    added commands to generate different reports using test scripts in package.json

--- env/qa.env.ts
+++ env/qa.env.ts
@@ -1,6 +1,6 @@
 export default {
-  baseURL: "http://localhost:7002",
-  //baseURL:"https://parabank.parasoft.com",
+  //baseURL: "http://localhost:7002",
+  baseURL:"https://parabank.parasoft.com",
   username: "demo",
   password: "demo"
 };
