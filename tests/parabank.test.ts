b08ffaa265005bb6f9ecc27f2ae9298b688308d0
mfaisalkhatri <mohammadfaisalkhatri@gmail.com>
2026-07-31 08:25:05 +0530

    added code to get the response after navigating to the website

--- tests/parabank.test.ts
+++ tests/parabank.test.ts
@@ -1,22 +1,48 @@
-import { test, expect } from "../fixtures/app.fixture"
-
+import { test, expect } from "../fixtures/app.fixture";
 
 test.describe("Parabank End to End tests", () => {
+  let accountNumber: string;
 
-    let accountNumber: string;
+  test("should create a new savings account", async ({
+    page,
+    basePage,
+    openNewAccountPage,
+  }) => {
+    await basePage.navigateTo("/parabank/overview.htm");
 
-    test("should create a new savings account", async ({ basePage, openNewAccountPage }) => {
+    await basePage.openMenu("Open New Account");
 
-        await basePage.navigateTo("/parabank/overview.htm");
+    await expect(openNewAccountPage.pageHeader).toBeVisible();
 
-        await basePage.openMenu("Open New Account");
-
-        await expect(openNewAccountPage.pageHeader).toBeVisible();
-
-        await openNewAccountPage.openNewAccount("SAVINGS");
-        await expect(openNewAccountPage.successMessageText).toBeVisible();
-
-        accountNumber = await openNewAccountPage.getAccountNumber();
-        console.log(accountNumber);
+    await page.on("response", async (response) => {
+      console.log(
+        response.status(),
+        response.url(),
+        response.request().method(),
+      );
     });
+
+    const responsePromise = page.waitForResponse(
+      (response) =>
+        response.url().includes("/accounts") &&
+        response.request().method() === "GET",
+    );
+
+    const response = await responsePromise;
+    console.log({
+      url: response.url(),
+      status: response.status(),
+      statusText: response.statusText(),
+      headers: await response.allHeaders(),
+      body: await response.json(),
+    });
+
+    console.log(await response.text());
+
+    await openNewAccountPage.openNewAccount("SAVINGS");
+    await expect(openNewAccountPage.successMessageText).toBeVisible();
+
+    accountNumber = await openNewAccountPage.getAccountNumber();
+    console.log(accountNumber);
+  });
 });
