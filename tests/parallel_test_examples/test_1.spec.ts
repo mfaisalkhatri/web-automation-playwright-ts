a48e122bffc75e1a072adc9db23c67873c5d01b3
mfaisalkhatri <mohammadfaisalkhatri@gmail.com>
2026-08-04 00:14:14 +0530

    added multiple test example for parallel tests

---
+++ tests/parallel_test_examples/test_1.spec.ts
@@ -0,0 +1,5 @@
+import { test } from "@playwright/test";
+
+test("Example test 1", async ({}) => {
+  console.log("Test one...");
+});
---
+++ tests/parallel_test_examples/test_2.spec.ts
@@ -0,0 +1,5 @@
+import { test } from "@playwright/test";
+
+test("Example test 2", async ({}) => {
+  console.log("Test two...");
+});
