54de8e6ae24ce99f1ae27432a8b9318a07d55eee
mfaisalkhatri <mohammadfaisalkhatri@gmail.com>
2026-08-27 18:48:28 +0530

    added td login button case

---
+++ tests/td.login.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from "@playwright/test";
+
+test("should click on the Easy Web login button", async ({ page }) => {
+  await page.goto("https://www.td.com/ca/en/personal-banking");
+  //await page.getByLabel("EasyWeb: Online Banking Login").click();
+  await page.getByRole('link', {name: "EasyWeb: Online Banking Login"}).click();
+
+  await expect(
+    page.getByRole("heading", { name: "EasyWeb Login" }),
+  ).toBeVisible();
+});
+
+test("should click on the Web broker login button", async ({ page }) => {
+  await page.goto("https://www.td.com/ca/en/personal-banking");
+  //await page.getByLabel("WebBroker Online Trading Login").click();
+  await page.getByRole('link', {name: "WebBroker Online Trading Login"}).click();
+  await expect(
+    page.getByRole("heading", { name: "WebBroker Login" }),
+  ).toBeVisible();
+});
